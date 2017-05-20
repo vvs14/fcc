e3e74f90f225864a9858aab6e7dcbb8a1e8ab91e
Vivek <vivekvsingh14@gmail.com>
2017-05-21 01:19:02 +0530

    made dropdown work on hover, background and text of dropdown on hover and wrote better jQuery code to handle project clicking from navbar

--- frontend/scripts/portfolioApp.js
+++ frontend/scripts/portfolioApp.js
@@ -1,16 +1,25 @@
 $(document).ready(function () {
-  //Tribute Project
-  $("#dropdownTributeAnchor").click(function () {
+  
+  $("#dropdownTributeAnchor, #dropdownConfusionAnchor").click(function(){
+    var anchorClicked =  $(this).attr('id');
+    $("div div div ul li").removeClass("active");
+    $(".tab-pane").removeClass("active");
+    if(anchorClicked==="dropdownTributeAnchor"){
+      $("#tributeTabListItem").addClass("active");
+      $("#tributeProject").addClass("active in");
+    }
+    else {
+      $("#confusionTabListItem").addClass("active");
+      $("#confusionProject").addClass("active in");
+    }
+  });
+
+  //When clicked on home or brand
+  $("#homeAnchor,#brandAnchor").click(function(){
     $("div div div ul li").removeClass("active");
     $(".tab-pane").removeClass("active");
     $("#tributeTabListItem").addClass("active");
     $("#tributeProject").addClass("active in");
   });
-  //ConfusionProject
-  $("#dropdownConfusionAnchor").click(function(){
-    $("div div div ul li").removeClass("active");
-    $(".tab-pane").removeClass("active");
-    $("#confusionTabListItem").addClass("active");
-    $("#confusionProject").addClass("active in");
-  });
+  
 });
