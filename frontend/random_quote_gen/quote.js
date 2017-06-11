fda3915fabddb7917d66e48010b1fae94dee29ba
Vivek <vivekvsingh14@gmail.com>
2017-06-11 23:36:10 +0530

    Added comments for documentation and refactored code more

--- frontend/random_quote_gen/quote.js
+++ frontend/random_quote_gen/quote.js
@@ -1,33 +1,53 @@
+//http://usejsdoc.org/about-getting-started.html
+/** 
+ * Function to get background color for box and body
+ */
 function getBackgroundColor() {
-  var boxColors = ["Blue","Brown","BurlyWood","CadetBlue","Chocolate","Coral","CornflowerBlue","Crimson","DarkBlue","DarkCyan","DarkGoldenRod","DarkGray","DarkGrey","DarkGreen","DarkKhaki","DarkOliveGreen","Darkorange","DarkRed","DarkSalmon","DarkSeaGreen","DarkSlateBlue","DarkSlateGray","DarkSlateGrey","DarkViolet","DeepSkyBlue","DimGray","DimGrey","DodgerBlue","FireBrick","ForestGreen","GoldenRod","Gray","Grey","Green","IndianRed","Indigo","LightCoral","LightSalmon","LightSeaGreen","LightSkyBlue","LightSteelBlue","Maroon","MediumSeaGreen","MediumSlateBlue","MediumTurquoise","MediumVioletRed","MidnightBlue","Navy","Olive","OliveDrab","Orange","PaleVioletRed","Peru","Plum","PowderBlue","Purple","RosyBrown","RoyalBlue","SaddleBrown","Salmon","SandyBrown","SeaGreen","Sienna","SkyBlue","SlateBlue","SlateGray","SlateGrey","SteelBlue","Tan","Teal","Tomato","Turquoise","Violet"];
-  return boxColors[Math.floor(Math.random() * boxColors.length)];
+    var boxColors = ["Blue", "Brown", "BurlyWood", "CadetBlue", "Chocolate", "Coral", "CornflowerBlue", "Crimson", "DarkBlue", "DarkCyan", "DarkGoldenRod", "DarkGray", "DarkGrey", "DarkGreen", "DarkKhaki", "DarkOliveGreen", "DarkRed", "DarkSalmon", "DarkSeaGreen", "DarkSlateBlue", "DarkSlateGray", "DarkSlateGrey", "DarkViolet", "DeepSkyBlue", "DimGray", "DimGrey", "DodgerBlue", "FireBrick", "ForestGreen", "GoldenRod", "Gray", "Grey", "Green", "IndianRed", "Indigo", "LightCoral", "LightSalmon", "LightSeaGreen", "LightSkyBlue", "LightSteelBlue", "Maroon", "MediumSeaGreen", "MediumSlateBlue", "MediumTurquoise", "MediumVioletRed", "MidnightBlue", "Olive", "OliveDrab", "Orange", "PaleVioletRed", "Peru", "Plum", "PowderBlue", "Purple", "RosyBrown", "RoyalBlue", "SaddleBrown", "Salmon", "SandyBrown", "SeaGreen", "Sienna", "SkyBlue", "SlateBlue", "SlateGray", "SlateGrey", "SteelBlue", "Tan", "Teal", "Tomato", "Turquoise"];
+    return boxColors[Math.floor(Math.random() * boxColors.length)];
 }
 
-$(document).ready( function() {
-  //To get new random quote from random quote API.
-  $("#getNewQuoteBtn").click( function() {
-    var request = new XMLHttpRequest();
-    $.ajax( {
-      beforeSend: function(request) {
-        request.setRequestHeader("X-Mashape-Key","4NdvxRO66gmshRekE7U8D6WOrfdQp1QjawIjsn4U3iclF7Vnmy");
-        request.setRequestHeader("Accept","application/json");
-      },
-      type: "GET",
-      url: "https://andruxnet-random-famous-quotes.p.mashape.com/?cat=famous&count=1",
-      processData: true,
-      dataType: "json",
-      success: function(responseData){
-        /*console.log(responseData);
-        console.log(responseData.author);*/
-        var backgroundColor = getBackgroundColor();
-        $("#quotePara").html("<q>" + responseData.quote+ "</q>");
-        $(".author").text("-"+responseData.author);
-        /*$("#centerBox").css("background-color", boxColor).fadeIn(1000);*/
-        $("#centerBox").css("background-color", backgroundColor);
-        $("body").css("background-color", backgroundColor);
-        console.log(backgroundColor);
-      },
-      error: function(req, err){ console.log('Some error occured here ' + err); }
+/** 
+ * Function to change view
+ * @param {ajaxResponse} responseData
+ * @param {string} backgroundColor - Background color to be used for center box and body
+ */
+function changeView(responseData, backgroundColor) {
+    $("#quotePara").html("<q>" + responseData.quote + "</q>");
+    $(".author").text("-" + responseData.author);
+    $("#centerBox").css("background-color", backgroundColor);
+    $("body").css("background-color", backgroundColor);
+}
+
+/**
+ * Function to change view when newQuoteBtn is clicked.
+ */
+$(document).ready(function() {
+    /**
+     * Function to get new random quote from random quote API.
+     */
+    $("#getNewQuoteBtn").click(function() {
+        var request = new XMLHttpRequest();
+        $.ajax({
+            beforeSend: function(request) {
+                request.setRequestHeader("X-Mashape-Key", "4NdvxRO66gmshRekE7U8D6WOrfdQp1QjawIjsn4U3iclF7Vnmy");
+                request.setRequestHeader("Accept", "application/json");
+            },
+            type: "GET",
+            url: "https://andruxnet-random-famous-quotes.p.mashape.com/?cat=famous&count=1",
+            processData: true,
+            dataType: "json",
+            success: function(responseData) {
+                var backgroundColor = getBackgroundColor();
+                changeView(responseData, backgroundColor);
+                console.log(responseData.author);
+                console.log(backgroundColor);
+            },
+            error: function(req, err) {
+                console.log('Some error occured here ' + err);
+            }
+        });
     });
-  });
+
+
 });
